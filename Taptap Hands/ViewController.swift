4229341a7c7547f57e426f5625709217a2e3b026
Jackie <jackiechoi66@gmail.com>
2017-06-14 14:58:57 +0200

    Initial Commit

---
+++ Taptap Hands/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Taptap Hands
+//
+//  Created by Jackie Choi on 6/14/17.
+//  Copyright © 2017 Jackie Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
